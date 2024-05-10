adb8c47ca1b945c7442f7e1186c2eb8c25c4e46a
Macley Sousa <macleysousa@hotmail.com>
2024-05-10 09:50:42 -0300

    chore: Add fast-xml-parser npm dependency

--- src/modules/schedule/schedule.service.ts
+++ src/modules/schedule/schedule.service.ts
@@ -1,13 +1,13 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
 import { DealernetService } from 'src/dealernet/dealernet.service';
+import { DealernetSchedule } from 'src/dealernet/schedule/response/schedule-response';
 import { IntegrationResponse } from 'src/petroplay/integration/entities/integration.entity';
+import { CreateOrderDto } from 'src/petroplay/order/dto/create-order.dto';
 import { PetroplayService } from 'src/petroplay/petroplay.service';
 
 import { ScheduleFilter } from './filters/schedule.filters';
-import { DealernetSchedule } from 'src/dealernet/schedule/response/schedule-response';
-import { CreateOrderDto } from 'src/petroplay/order/dto/create-order.dto';
 
 @Injectable()
 export class ScheduleService {
@@ -21,49 +21,59 @@ export class ScheduleService {
   async sync(filter: ScheduleFilter): Promise<void> {
     const integrations = await this.petroplay.integration.find({ clients: filter.client_ids });
     if (!integrations) throw new BadRequestException('Integration not found');
+
+    const orders = await this.schema(filter);
+    for await (const order of orders.chunk(10)) {
+      Logger.warn(`Sending ${order.length} orders to Petroplay`);
+      await this.petroplay.order.import(order);
+    }
   }
 
-  async schema(filter: ScheduleFilter): Promise<unknown> {
+  async schema(filter: ScheduleFilter): Promise<CreateOrderDto[]> {
     const integrations = await this.petroplay.integration.find({ clients: filter.client_ids });
     if (!integrations) throw new BadRequestException('Integration not found');
 
-    const orders = [];
+    const orders: CreateOrderDto[] = [];
     for await (const integration of integrations) {
       const schedules = await this.dialernet.schedule.find(integration.dealernet, filter.start_date, filter.end_date);
-      orders.push(await this.scheduleToOs(integration, schedules));
+      await this.scheduleToOs(integration, schedules).then((data) => orders.push(...data));
     }
 
     return orders;
   }
 
-  async scheduleToOs(integration: IntegrationResponse, schedules: DealernetSchedule[]): Promise<unknown> {
+  async scheduleToOs(integration: IntegrationResponse, schedules: DealernetSchedule[]): Promise<CreateOrderDto[]> {
     const orders: CreateOrderDto[] = [];
     for await (const schedule of schedules) {
       const customer = await this.dialernet.customer.findById(integration.dealernet, schedule.ClienteCodigo);
       const address = customer.Endereco?.EnderecoItem;
-      const vehicle = await this.dialernet.vehicle.findVehicleByPlate(integration.dealernet, schedule.VeiculoPlaca);
+      const phone = customer.Telefone?.TelefoneItem[0];
+      const vehicle: any = {}; //await this.dialernet.vehicle.findVehicleByPlate(integration.dealernet, schedule.VeiculoPlaca);
 
       const dto: CreateOrderDto = {
         clientId: integration.client_id,
         customerName: schedule.ClienteNome,
         customerDocument: schedule.ClienteDocumento.toString(),
         phoneType: 'PHON',
-        phoneNumber: customer.Telefone?.TelefoneItem[0]?.PessoaTelefone_Fone?.toString(),
+        phoneNumber: phone?.PessoaTelefone_Fone?.toString(),
         email: customer.Pessoa_Email,
-        addressName: address.PessoaEndereco_TipoEndereco?.trim(),
-        city: address.PessoaEndereco_Cidade?.trim(),
-        state: address.PessoaEndereco_Estado?.trim(),
-        neighborhood: address.PessoaEndereco_Bairro?.trim(),
-        postal_code: address.PessoaEndereco_CEP?.toString()?.trim(),
-        street: address.PessoaEndereco_Logradouro?.trim(),
-        complement: address.PessoaEndereco_Complemento?.trim(),
-        number: address.PessoaEndereco_Numero?.toString()?.trim(),
-        maker: vehicle.VeiculoMarca_Descricao,
-        model: schedule.VeiculoModelo,
-        version: undefined,
-        year: undefined,
-        fuel: undefined,
-        color: vehicle.Veiculo_CorExternaDescricao,
+        addressName: address?.PessoaEndereco_TipoEndereco?.trim(),
+        city: address?.PessoaEndereco_Cidade?.trim(),
+        state: address?.PessoaEndereco_Estado?.trim(),
+        neighborhood: address?.PessoaEndereco_Bairro?.trim(),
+        postal_code: address?.PessoaEndereco_CEP?.toString()?.trim(),
+        street: address?.PessoaEndereco_Logradouro?.trim(),
+        complement: address?.PessoaEndereco_Complemento?.trim(),
+        number: address?.PessoaEndereco_Numero?.toString()?.trim(),
+        // maker_id: undefined,
+        // maker: vehicle.VeiculoMarca_Descricao,
+        // model_id: undefined,
+        // model: schedule.VeiculoModelo,
+        // version_id: undefined,
+        // version: undefined,
+        // year: undefined,
+        // fuel: undefined,
+        // color: vehicle.Veiculo_CorExternaDescricao,
         licensePlate: schedule.VeiculoPlaca?.trim().replace('-', '').substring(0, 7) ?? 'BR00000',
         mileage: Number(schedule.VeiculoKM) ?? 0,
         chassisNumber: schedule.VeiculoChassi ?? 'Não Informado',
--- src/petroplay/order/order.service.ts
+++ src/petroplay/order/order.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
+
+import { petroplay } from 'src/commons/web-client';
 
 @Injectable()
-export class PetroplayOrderService {}
+export class PetroplayOrderService {
+  async import(order: any): Promise<void> {
+    const client = await petroplay.v1();
+    await client.post('/v1/orders/import', order).catch((err) => {
+      Logger.error('Error on import order:', err, 'PetroplayOrderService.import');
+      throw new BadRequestException('Error on import order');
+    });
+  }
+}
