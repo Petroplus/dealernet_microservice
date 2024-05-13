8b7e2275b5dc3dafbc98a5954daa1c3f775f1104
Macley Sousa <macleysousa@hotmail.com>
2024-05-13 15:49:50 -0300

    chore: Update customer controller to handle create, find, and update operations
    The `CustomerController` in `customer.controller.ts` has been updated to include new methods for creating, finding, and updating customers. These changes allow for better interaction with the `CustomerService` and improve the overall functionality of the controller.

--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -1,8 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put, Query } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 import { CustomerService } from './customer.service';
+import { CustomerFilter } from './filters/customer.filter';
 
 @Controller('customers')
 export class CustomerController {
   constructor(private readonly service: CustomerService) {}
+
+  @Post()
+  @ApiResponse({ status: 200 })
+  async create(@Param('client_id', ParseUUIDPipe) client_id: string, @Body() data: unknown): Promise<unknown> {
+    return; //this.service.create(client_id, data);
+  }
+
+  @Get()
+  @ApiResponse({ status: 200 })
+  async find(@Param('client_id', ParseUUIDPipe) client_id: string, @Query() filter: CustomerFilter): Promise<unknown> {
+    return this.service.find(client_id, filter);
+  }
+
+  // @Get(':id')
+  // @ApiResponse({ status: 200 })
+  // async findById(@Param('client_id', ParseUUIDPipe) client_id: string, @Param('id', ParseUUIDPipe) id: string): Promise<unknown> {
+  //   return this.service.findById(client_id, id);
+  // }
+
+  @Put(':id')
+  @ApiResponse({ status: 200 })
+  async update(
+    @Param('client_id', ParseUUIDPipe) client_id: string,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() data: unknown
+  ): Promise<unknown> {
+    return; //this.service.update(client_id, id, data);
+  }
 }
