3f01bac8c1dfc462ca07a78ace6c923eef7febe0
Caio-Pedras <caio.vp@gmail.com>
2024-05-14 13:37:24 -0300

    customer crud

--- src/dealernet/customer/customer.service.ts
+++ src/dealernet/customer/customer.service.ts
@@ -6,6 +6,7 @@ import { dealernet } from 'src/commons/web-client';
 import { IntegrationDealernet } from 'src/petroplay/integration/entities/integration.entity';
 
 import { DealernetCustomerResponse } from './response/customers.response';
+import { CreateCustomerDTO } from '../dto/create-customer.dto';
 
 @Injectable()
 export class DealernetCustomerService {
@@ -85,6 +86,186 @@ export class DealernetCustomerService {
       }
     } catch (error) {
       Logger.error('Erro ao fazer a requisição:', error, 'DealernetCustomerService.findById');
+      throw error;
+    }
+  }
+
+  async createCustomer(connection: IntegrationDealernet, dto: CreateCustomerDTO) {
+    const url = `$${connection.url}/aws_fastserviceapi.aspx`;
+    const xmlBody = `
+            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:deal="DealerNet">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <deal:WS_FastServiceApi.PESSOA>
+                    <deal:Usuario>${connection.user}</deal:Usuario>
+                    <deal:Senha>${connection.key}</deal:Senha>
+                    <deal:Sdt_fspessoain>
+                        <deal:Pessoa_Nome>${dto.Pessoa_Nome ?? '?'}</deal:Pessoa_Nome>
+                        <deal:Pessoa_TipoPessoa>${dto.Pessoa_TipoPessoa ?? '?'}</deal:Pessoa_TipoPessoa>
+                        <deal:Pessoa_DocIdentificador>${dto.Pessoa_DocIdentificador ?? '?'}</deal:Pessoa_DocIdentificador>
+                        <deal:Pessoa_RG>${dto.Pessoa_RG_InscricaoEstadual ?? '?'}</deal:Pessoa_RG>
+                        <deal:Pessoa_OrgaoEmissor>${dto.Pessoa_OrgaoEmissor ?? '?'}</deal:Pessoa_OrgaoEmissor>
+                        <deal:Pessoa_InscricaoSuframa>?</deal:Pessoa_InscricaoSuframa>
+                        <deal:Pessoa_Sexo>${dto.Pessoa_Sexo ?? '?'}</deal:Pessoa_Sexo>
+                        <deal:Pessoa_Email>${dto.Pessoa_Email ?? '?'}</deal:Pessoa_Email>
+                        <deal:Pessoa_Nacionalidade>?</deal:Pessoa_Nacionalidade>
+                        <deal:Pessoa_EstadoCivil>?</deal:Pessoa_EstadoCivil>
+                        <deal:Pessoa_Nascimento>${dto.Pessoa_Nascimento ?? '?'}</deal:Pessoa_Nascimento>
+                        <deal:Acao>INC</deal:Acao>
+                        <deal:Endereco>
+                            <deal:EnderecoItem>
+                                <deal:PessoaEndereco_TipoEndereco>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_TipoEndereco ?? '?'
+                                }</deal:PessoaEndereco_TipoEndereco>
+                                <deal:PessoaEndereco_Logradouro>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Logradouro ?? '?'
+                                }</deal:PessoaEndereco_Logradouro>
+                                <deal:PessoaEndereco_TipoLogradouro_Descricao>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_TipoLogradouro_Descricao ?? '?'
+                                }</deal:PessoaEndereco_TipoLogradouro_Descricao>
+                                <deal:PessoaEndereco_Numero>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Numero ?? '?'
+                                }</deal:PessoaEndereco_Numero>
+                                <deal:PessoaEndereco_Complemento>?
+      }</deal:PessoaEndereco_Complemento>
+                                <deal:PessoaEndereco_CEP>${dto.Endereco?.EnderecoItem?.PessoaEndereco_CEP ?? '?'}</deal:PessoaEndereco_CEP>
+                                <deal:PessoaEndereco_Bairro>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Bairro ?? '?'
+                                }</deal:PessoaEndereco_Bairro>
+                                <deal:PessoaEndereco_Cidade>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Cidade ?? '?'
+                                }</deal:PessoaEndereco_Cidade>
+                                <deal:PessoaEndereco_Estado>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Estado ?? '?'
+                                }</deal:PessoaEndereco_Estado>
+                            </deal:EnderecoItem>
+                        </deal:Endereco>
+                        <deal:Telefone>
+                            <deal:TelefoneItem>
+                                <deal:PessoaTelefone_TipoTelefone>${
+                                  dto.Telefone?.TelefoneItem?.PessoaTelefone_TipoTelefone ?? '?'
+                                }</deal:PessoaTelefone_TipoTelefone>
+                                <deal:PessoaTeleFone_DDD>${dto.Telefone?.TelefoneItem?.PessoaTeleFone_DDD ?? '?'}</deal:PessoaTeleFone_DDD>
+                                <deal:PessoaTelefone_Fone>${
+                                  dto.Telefone?.TelefoneItem?.PessoaTelefone_Fone ?? '?'
+                                }</deal:PessoaTelefone_Fone>
+                            </deal:TelefoneItem>
+                        </deal:Telefone>
+                        <deal:MeioContato>
+                            <deal:MeioContatoItem>
+                                <deal:PessoaMeioContato_Tipo>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Ativo ?? '?'
+                                }</deal:PessoaMeioContato_Tipo>
+                                <deal:PessoaMeioContato_Ativo>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Ativo ?? '?'
+                                }</deal:PessoaMeioContato_Ativo>
+                                <deal:PessoaMeioContato_Principal>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Principal ?? '?'
+                                }</deal:PessoaMeioContato_Principal>
+                            </deal:MeioContatoItem>
+                        </deal:MeioContato>
+                    </deal:Sdt_fspessoain>
+                </deal:WS_FastServiceApi.PESSOA>
+            </soapenv:Body>
+        </soapenv:Envelope>
+        `;
+    try {
+      const client = await dealernet();
+      const response = await client.post(url, xmlBody);
+      const xmlData = response.data;
+      console.log(xmlData);
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
+      throw error;
+    }
+  }
+
+  async updateCustomer(connection: IntegrationDealernet, dto: CreateCustomerDTO) {
+    const url = `$${connection.url}/aws_fastserviceapi.aspx`;
+    const xmlBody = `
+            <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:deal="DealerNet">
+            <soapenv:Header/>
+            <soapenv:Body>
+                <deal:WS_FastServiceApi.PESSOA>
+                    <deal:Usuario>${connection.user}</deal:Usuario>
+                    <deal:Senha>${connection.key}</deal:Senha>
+                    <deal:Sdt_fspessoain>
+                        <deal:Pessoa_Nome>${dto.Pessoa_Nome ?? '?'}</deal:Pessoa_Nome>
+                        <deal:Pessoa_TipoPessoa>${dto.Pessoa_TipoPessoa ?? '?'}</deal:Pessoa_TipoPessoa>
+                        <deal:Pessoa_DocIdentificador>${dto.Pessoa_DocIdentificador ?? '?'}</deal:Pessoa_DocIdentificador>
+                        <deal:Pessoa_RG>${dto.Pessoa_RG_InscricaoEstadual ?? '?'}</deal:Pessoa_RG>
+                        <deal:Pessoa_OrgaoEmissor>${dto.Pessoa_OrgaoEmissor ?? '?'}</deal:Pessoa_OrgaoEmissor>
+                        <deal:Pessoa_InscricaoSuframa>?</deal:Pessoa_InscricaoSuframa>
+                        <deal:Pessoa_Sexo>${dto.Pessoa_Sexo ?? '?'}</deal:Pessoa_Sexo>
+                        <deal:Pessoa_Email>${dto.Pessoa_Email ?? '?'}</deal:Pessoa_Email>
+                        <deal:Pessoa_Nacionalidade>?</deal:Pessoa_Nacionalidade>
+                        <deal:Pessoa_EstadoCivil>?</deal:Pessoa_EstadoCivil>
+                        <deal:Pessoa_Nascimento>${dto.Pessoa_Nascimento ?? '?'}</deal:Pessoa_Nascimento>
+                        <deal:Acao>ALT</deal:Acao>
+                        <deal:Endereco>
+                            <deal:EnderecoItem>
+                                <deal:PessoaEndereco_TipoEndereco>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_TipoEndereco ?? '?'
+                                }</deal:PessoaEndereco_TipoEndereco>
+                                <deal:PessoaEndereco_Logradouro>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Logradouro ?? '?'
+                                }</deal:PessoaEndereco_Logradouro>
+                                <deal:PessoaEndereco_TipoLogradouro_Descricao>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_TipoLogradouro_Descricao ?? '?'
+                                }</deal:PessoaEndereco_TipoLogradouro_Descricao>
+                                <deal:PessoaEndereco_Numero>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Numero ?? '?'
+                                }</deal:PessoaEndereco_Numero>
+                                <deal:PessoaEndereco_Complemento>?
+      }</deal:PessoaEndereco_Complemento>
+                                <deal:PessoaEndereco_CEP>${dto.Endereco?.EnderecoItem?.PessoaEndereco_CEP ?? '?'}</deal:PessoaEndereco_CEP>
+                                <deal:PessoaEndereco_Bairro>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Bairro ?? '?'
+                                }</deal:PessoaEndereco_Bairro>
+                                <deal:PessoaEndereco_Cidade>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Cidade ?? '?'
+                                }</deal:PessoaEndereco_Cidade>
+                                <deal:PessoaEndereco_Estado>${
+                                  dto.Endereco?.EnderecoItem?.PessoaEndereco_Estado ?? '?'
+                                }</deal:PessoaEndereco_Estado>
+                            </deal:EnderecoItem>
+                        </deal:Endereco>
+                        <deal:Telefone>
+                            <deal:TelefoneItem>
+                                <deal:PessoaTelefone_TipoTelefone>${
+                                  dto.Telefone?.TelefoneItem?.PessoaTelefone_TipoTelefone ?? '?'
+                                }</deal:PessoaTelefone_TipoTelefone>
+                                <deal:PessoaTeleFone_DDD>${dto.Telefone?.TelefoneItem?.PessoaTeleFone_DDD ?? '?'}</deal:PessoaTeleFone_DDD>
+                                <deal:PessoaTelefone_Fone>${
+                                  dto.Telefone?.TelefoneItem?.PessoaTelefone_Fone ?? '?'
+                                }</deal:PessoaTelefone_Fone>
+                            </deal:TelefoneItem>
+                        </deal:Telefone>
+                        <deal:MeioContato>
+                            <deal:MeioContatoItem>
+                                <deal:PessoaMeioContato_Tipo>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Ativo ?? '?'
+                                }</deal:PessoaMeioContato_Tipo>
+                                <deal:PessoaMeioContato_Ativo>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Ativo ?? '?'
+                                }</deal:PessoaMeioContato_Ativo>
+                                <deal:PessoaMeioContato_Principal>${
+                                  dto.MeioContato?.MeioContatoItem?.PessoaMeioContato_Principal ?? '?'
+                                }</deal:PessoaMeioContato_Principal>
+                            </deal:MeioContatoItem>
+                        </deal:MeioContato>
+                    </deal:Sdt_fspessoain>
+                </deal:WS_FastServiceApi.PESSOA>
+            </soapenv:Body>
+        </soapenv:Envelope>
+        `;
+    try {
+      const client = await dealernet();
+      const response = await client.post(url, xmlBody);
+      const xmlData = response.data;
+      console.log(xmlData);
+    } catch (error) {
+      console.error('Erro ao fazer a requisição:', error);
       throw error;
     }
   }
--- src/modules/customer/customer.controller.ts
+++ src/modules/customer/customer.controller.ts
@@ -1,23 +1,31 @@
 import { Body, Controller, Get, Param, ParseUUIDPipe, Post, Put, Query } from '@nestjs/common';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { CustomerService } from './customer.service';
 import { CustomerFilter } from './filters/customer.filter';
+import { CreateCustomerDTO } from 'src/dealernet/dto/create-customer.dto';
 
+@ApiTags('Customers')
 @Controller('customers')
 export class CustomerController {
   constructor(private readonly service: CustomerService) {}
 
-  @Post()
+  @Post(':client_id')
   @ApiResponse({ status: 200 })
-  async create(@Param('client_id', ParseUUIDPipe) client_id: string, @Body() data: unknown): Promise<unknown> {
-    return; //this.service.create(client_id, data);
+  async create(@Param('client_id', ParseUUIDPipe) client_id: string, @Body() dto: CreateCustomerDTO): Promise<void> {
+    return this.service.create(client_id, dto);
   }
 
-  @Get()
+  @Get(':client_id')
   @ApiResponse({ status: 200 })
   async find(@Param('client_id', ParseUUIDPipe) client_id: string, @Query() filter: CustomerFilter): Promise<unknown> {
     return this.service.find(client_id, filter);
+  }
+
+  @Put(':client_id')
+  @ApiResponse({ status: 200 })
+  async update(@Param('client_id', ParseUUIDPipe) client_id: string, @Body() dto: CreateCustomerDTO): Promise<void> {
+    return this.service.update(client_id, dto);
   }
 
   // @Get(':id')
@@ -25,14 +33,4 @@ export class CustomerController {
   // async findById(@Param('client_id', ParseUUIDPipe) client_id: string, @Param('id', ParseUUIDPipe) id: string): Promise<unknown> {
   //   return this.service.findById(client_id, id);
   // }
-
-  @Put(':id')
-  @ApiResponse({ status: 200 })
-  async update(
-    @Param('client_id', ParseUUIDPipe) client_id: string,
-    @Param('id', ParseUUIDPipe) id: string,
-    @Body() data: unknown
-  ): Promise<unknown> {
-    return; //this.service.update(client_id, id, data);
-  }
 }
