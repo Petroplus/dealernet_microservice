84049c204aa03c539f24d33689e8e5c61692019e
Caio-Pedras <caio.vp@gmail.com>
2024-05-23 10:44:19 -0300

    import routes

--- src/petroplay/petroplay.module.ts
+++ src/petroplay/petroplay.module.ts
@@ -3,9 +3,10 @@ import { DynamicModule, Module } from '@nestjs/common';
 import { PetroplayIntegrationModule } from './integration/integration.module';
 import { PetroplayService } from './petroplay.service';
 import { PetroplayOrderModule } from './order/order.module';
+import { PetroplayCustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [PetroplayIntegrationModule, PetroplayOrderModule],
+  imports: [PetroplayIntegrationModule, PetroplayOrderModule, PetroplayCustomerModule],
   providers: [PetroplayService],
   exports: [PetroplayService],
 })
--- src/petroplay/petroplay.service.ts
+++ src/petroplay/petroplay.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 import { PetroplayIntegrationService } from './integration/integration.service';
 import { PetroplayOrderService } from './order/order.service';
+import { PetroplayCustomerService } from './customer/customer.service';
 
 @Injectable()
 export class PetroplayService {
   constructor(
     public readonly integration: PetroplayIntegrationService,
-    public readonly order: PetroplayOrderService
+    public readonly order: PetroplayOrderService,
+    public readonly customer: PetroplayCustomerService
   ) {}
 }
