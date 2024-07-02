78c23357cab3ed62b33d2f1d6a5eebe783c46542
Caio-Pedras <caio.vp@gmail.com>
2024-07-02 11:53:19 -0300

    stash

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -120,40 +120,6 @@ export class OrderService {
       })
     })
 
-    // const products: ProdutoCreateDTO[] = order.items
-    //   .map(({ products }) => {
-    //     return products.map((item) => ({
-    //       tipo_os_sigla: order.os_type?.external_id,
-    //       produto_referencia: item.product.internal_id,
-    //       valor_unitario: item.product.price,
-    //       quantidade: item.quantity,
-    //     })) as ProdutoCreateDTO[];
-    //   })
-    //   .flatMap((item) => item);
-
-    //  order.items
-    //   .map(({ service }) => {
-    //     const tasks = service.items.filter((x) => x.entity_type == 'task').map((serviceItem) => serviceItem);
-    //     const productsAditional = service.items
-    //       .filter((x) => x.entity_type == 'product')
-    //       .map((serviceItem) => ({
-    //         tipo_os_sigla: order.os_type.external_id,
-    //         produto_referencia: serviceItem.product.internal_id,
-    //         valor_unitario: serviceItem.product.price,
-    //         quantidade: serviceItem.quantity,
-    //       }));
-
-    //     return tasks.map((item, index) => ({
-    //       tipo_os_sigla: order.os_type.external_id,
-    //       tmo_referencia: item.task.internal_id,
-    //       tempo: item.quantity,
-    //       valor_unitario: item.task.price,
-    //       quantidade: Math.ceil(item.quantity),
-    //       produtos: index == 0 ? [...products, ...productsAditional] : productsAditional,
-    //     })) as ServicoCreateDTO[];
-    //   })
-    //   .flatMap((item) => item);
-
     const OS: CreateOsDTO = {
       veiculo_placa_chassi: order.vehicle_chassis_number,
       veiculo_Km: Number(order.mileage) || 0,
@@ -177,6 +143,69 @@ export class OrderService {
     return OS;
   }
 
+  async OsDtoToDealenrtOsAppointments(order: PetroplayOrderEntity): Promise<CreateOsDTO> {
+
+    const orderBudget = await this.petroplay.order.findOrderBudget(order.id)
+    const products: ProdutoCreateDTO[] = []
+    const services: ServicoCreateDTO[] = []
+
+    let aux_os_type =  order?.os_type?.external_id
+    orderBudget.map(budget=>{
+      if(!aux_os_type){
+        aux_os_type= budget?.os_type?.external_id
+      }
+      budget.products.map(product=>{
+        if(!aux_os_type){
+          aux_os_type=  product?.os_type?.external_id
+        }
+          const tipo_os_sigla = product?.os_type?.external_id || budget?.os_type?.external_id ||  order?.os_type?.external_id
+          products.push({
+            tipo_os_sigla,
+            produto_referencia: product.integration_id,
+            valor_unitario: product.price,
+            quantidade: product.quantity,
+          })
+      })
+
+      budget.services.map((service,index )=>{
+        if(!aux_os_type){
+          aux_os_type= service?.os_type?.external_id
+        }
+        const tipo_os_sigla = service?.os_type?.external_id || budget?.os_type?.external_id ||  order?.os_type?.external_id
+        services.push({
+          tipo_os_sigla,
+          tmo_referencia: service.integration_id,
+          tempo: service.quantity,
+          valor_unitario: service.price,
+          quantidade: Math.ceil(service.quantity),
+          produtos: index == 0 ? [...products] : [],
+        })
+      })
+    })
+
+    const OS: CreateOsDTO = {
+      veiculo_placa_chassi: order.vehicle_chassis_number,
+      veiculo_Km: Number(order.mileage) || 0,
+      cliente_documento: order.customer_document,
+      consultor_documento: await this.formatarDoc(order.consultant?.cod_consultor),
+      data: new Date(order.inspection).toISOString(),
+      data_final: new Date().toISOString(),
+      data_prometida: new Date(order.conclusion).toISOString(),
+      nro_prisma: order.prisma,
+      observacao: order.notes,
+      prisma_codigo: order.prisma,
+      tipo_os_sigla: aux_os_type,
+      servicos: services,
+      tipo_os: {
+        tipo_os_item: {
+          tipo_os_sigla: aux_os_type,
+          consultor_documento: await this.formatarDoc(order.consultant?.cod_consultor),
+        },
+      },
+    };
+    return OS;
+
+  }
   async formatarDoc(doc?: string): Promise<string> {
     if (!doc) return '?';
     if (doc?.length === 11) return doc;
